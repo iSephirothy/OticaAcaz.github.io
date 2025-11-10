a2ee8885bc3a27c2b9fd2c6093e3e5125a8e04cc
iSephirothy <sephiroth0305@outlook.com.br>
2025-11-10 14:42:40 -0300

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Exemplo: mudar banner automaticamente
+let banners = [
+  "Bem-vindo à Ótica Acaz",
+  "Promoções incríveis todo mês",
+  "Os melhores óculos com estilo"
+];
+
+let bannerText = document.querySelector(".banner h1");
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % banners.length;
+  bannerText.textContent = banners[index];
+}, 4000);
